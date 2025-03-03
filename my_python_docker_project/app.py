4a42cd969d33a04e3bc8d3b516d79fc4ec5407bb
Souravthakur21 <souravt01hakur0786@gmail.com>
2025-03-03 11:39:44 +0530

    Monday COmmit

--- my_python_docker_project/app.py
+++ my_python_docker_project/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return " yessssssssssssssssssssssssssssss "
+    return " How are you "
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
